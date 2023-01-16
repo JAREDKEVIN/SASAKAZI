4d91e75fc1a6c101f9e52dcbb1cc7e6f8728f503
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2023-01-16 18:00:30 +0300

    database configurations to sql

--- mypro/settings.py
+++ mypro/settings.py
@@ -73,14 +73,16 @@ WSGI_APPLICATION = 'mypro.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crud',
+        'HOST': '127.0.0.1',
+        'PORT': '8000',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
